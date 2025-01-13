550db4aa74a63afeb0ed4e48d17d3e65b0595264
Adithya Ekanayaka <adithyasean@outlook.com>
2025-01-14 00:04:56 +0530

    Initial commit

---
+++ src/test/java/com/nextstep/education/NextStepEducationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nextstep.education;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NextStepEducationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
